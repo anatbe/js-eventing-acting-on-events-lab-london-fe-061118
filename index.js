2fb12e8a61bc8536b37d34197e4c1e3f6ae210a7
Anat Benski Lipman <anat_benski@hotmail.com>
2018-08-06 19:43:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(righttNumbers, 10)
+  var right = parseInt(rightNumbers, 10)
  
   if (left > 0) {
     dodger.style.right = `${right - 1}px`
